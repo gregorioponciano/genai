9e7948c7b8a8c5cb7c0ad1b537684f565ac11e10
Gregório Ponciano <gregorioponci.91@gmail.com>
2026-05-23 19:51:29 -0300

    genai

---
+++ index.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+
+client = genai.Client()
+
+response = client.models.generate_content(
+    model='gemini-2.5-flash',
+    contents='ola boa noite.',
+)
+
+print(response.text)
+
